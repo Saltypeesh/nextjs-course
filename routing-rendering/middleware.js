f8495150104fc0832d27e85b5bd64f34b765173d
Saltypeesh <tungthanh254@gmail.com>
2025-03-21 02:12:25 +0700

    Complete Routing & Page Rendering
    - Refactor layout structure by removing unused components
    - Simplifying RootLayout; update MainHeader to use NavLink for navigation.
    - Clean up code formatting for consistency.

---
+++ routing-rendering/middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+
+  console.log(request)
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/news',
+}
+
