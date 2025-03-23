ccf3c450cbbac0364c9f70c89426f5522a1de9a8
Saltypeesh <tungthanh254@gmail.com>
2025-03-24 05:51:28 +0700

    Enhance configuration and component updates for improved image handling and metadata
    - Update next.config.mjs to allow remote images from Cloudinary.
    - Add CSS styles for post images in globals.css.
    - Introduce metadata for the LatestPosts page in page.js.
    - Implement dynamic metadata generation for the FeedPage based on post count.
    - Replace img tag with Image component in Header for optimized image loading.
    - Update Post component to utilize Image for better image handling and quality control.

--- data-mutation/app/feed/page.js
+++ data-mutation/app/feed/page.js
@@ -1,6 +1,19 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'All posts',
+//   description: 'A list of all posts from all users',
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `All posts (${posts.length})`,
+    description: 'A list of all posts from all users',
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
